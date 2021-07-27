06a31e289adeca6db4bc38e5ea24c9a091149f48
PhilipeAnderson <philipeacampos@hotmail.com>
2021-07-27 13:16:12 -0300

    Iniciando projeto com a tela de Login

---
+++ app/src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Login = () => {
+    return(
+        <>
+            <Text>Página Login</Text>
+        </>
+    );
+};
+
+export default Login;
