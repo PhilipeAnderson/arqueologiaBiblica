742e642ea88656d828097b9f60c46afd32433817
PhilipeAnderson <philipeacampos@hotmail.com>
2021-07-31 23:03:51 -0300

    Adicionado o Linear Gradiente finalizada a Hero

--- app/src/components/Hero/index.js
+++ app/src/components/Hero/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Text, Image, View, TouchableOpacity } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import styles from './styles'
 
@@ -7,15 +8,18 @@ const Hero = () => {
     return (
         <View>
             <Image
-            style={styles.hero}
-            source={require('../../assets/comoForamFeitasTodasAsCoisas.png')}
+                style={styles.hero}
+                source={require('../../assets/comoForamFeitasTodasAsCoisas.png')}
             />
-            <TouchableOpacity>
-                <Text style={styles.textEstudo}>ENTRAR NO ESTUDO</Text>
-            </TouchableOpacity>
+            <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']}>
+                <TouchableOpacity>
+                    <Text style={styles.textEstudo}>ENTRAR NO ESTUDO</Text>
+                </TouchableOpacity>
+            </LinearGradient>
+
         </View>
-        
-       
+
+
     )
 }
 
