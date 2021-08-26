3827b528afebcc010842c5c86262e9b139b876be
Philipe Anderson De Campos <philipeacampos@gmail.com>
2021-08-26 13:05:49 -0300

    Criada o componente da sessao de Previas

---
+++ app/src/components/Previas/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { FlatList, TouchableOpacity, View, Image} from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+import styles from './styles'
+
+const Previas = () => {
+    return(
+        <FlatList
+                    horizontal
+                    style={styles.flatListContainer}
+                    data={[1, 2, 3, 4, 5, 6, 7]}
+                    renderItem={({ item, index }) => (
+                        <TouchableOpacity 
+                            key={index} 
+                            style={{
+                                marginLeft: index == 0 ? 20 : 0, 
+                                marginRight: 20
+                                }}
+                        >
+                            <View style={styles.oval}>
+                                <Image 
+                                    style={styles.capa}
+                                    source={{ uri: 'https://i.imgur.com/EJyDFeY.jpg' }} />
+
+                                <Image
+                                    resizeMode="contain"
+                                    style={styles.logo}
+                                    source={{ uri: 'https://i.imgur.com/oDHGhpX.png' }} />
+                            
+                                <LinearGradient
+                                    style={styles.gradient}
+                                    colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']}
+                                />
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                />
+    )
+}
+
+export const Previas
--- app/src/pages/Home/styles.js
+++ app/src/pages/Home/styles.js
@@ -18,33 +18,6 @@ const styles = StyleSheet.create({
         marginTop: 5,
         marginBottom: 50
     },
-    oval: {
-        backgroundColor: '#e50914',
-        padding: 2.5,
-        width: 90,
-        height: 90,
-        borderRadius: 90
-    },
-    capa: {
-        height: 85,
-        width: 85,
-        borderRadius: 85
-    },
-    logo: {
-        width: 90,
-        height: 45,
-        position: 'absolute',
-        zIndex: 10,
-        bottom: 10,
-        alignSelf: 'center'
-    },
-    gradient:{
-        width: '100%',
-        height: 80,
-        position: 'absolute',
-        zIndex: 9,
-        bottom: 0
-    },
     previaContainer:{
         width: '100%',
         marginTop: 30
