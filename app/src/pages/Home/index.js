19bfcb8c7ed998e458e925868aa4f1820e83c304
PhilipeAnderson <philipeacampos@hotmail.com>
2021-08-26 11:38:12 -0300

    Criada a sessào de previas dos estudos e foi adcionado o FlatList da lib reactNative

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import {View, Text, TouchableOpacity} from 'react-native'
+import { View, ScrollView, TouchableOpacity, FlatList, Image } from 'react-native'
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import styles from './styles'
-import { Button } from 'react-native-paper'
+import { Button, Title } from 'react-native-paper'
 
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
 import ButtonVertical from '../../components/ButtonVertical'
 
+import LinearGradient from 'react-native-linear-gradient'
+
 
 const Home = () => {
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <Header />
             <Hero />
 
@@ -29,7 +30,41 @@ const Home = () => {
                 <ButtonVertical icon="information-outline" text="Saiba Mais" />
             </View>
 
-        </View>
+            <View style={styles.previaContainer}>
+                <Title style={styles.previaTitle}>Prévias</Title>
+                <FlatList
+                    horizontal
+                    style={styles.flatListContainer}
+                    data={[1, 2, 3, 4, 5, 6, 7]}
+                    renderItem={({ item, index }) => (
+                        <TouchableOpacity 
+                            key={index} 
+                            style={{
+                                marginLeft: index == 0 ? 20 : 0, 
+                                marginRight: 20
+                                }}
+                        >
+                            <View style={styles.oval}>
+                                <Image 
+                                    style={styles.capa}
+                                    source={{ uri: 'https://i.imgur.com/EJyDFeY.jpg' }} />
+
+                                <Image
+                                    resizeMode="contain"
+                                    style={styles.logo}
+                                    source={{ uri: 'https://i.imgur.com/oDHGhpX.png' }} />
+                            
+                                <LinearGradient
+                                    style={styles.gradient}
+                                    colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']}
+                                />
+                            </View>
+                        </TouchableOpacity>
+                    )}
+                />
+            </View>
+
+        </ScrollView>
 
     );
 };
--- app/src/pages/Home/styles.js
+++ app/src/pages/Home/styles.js
@@ -8,9 +8,49 @@ const styles = StyleSheet.create({
     menuHeader: {
         width: '100%',
         height: 38,
-        marginTop: 100,
+        marginTop: 80,
         flexDirection: 'row',
         justifyContent: 'space-evenly'
+    },
+    flatListContainer: {
+        width: '100%',
+        height: 100,
+        marginTop: 5,
+        marginBottom: 50
+    },
+    oval: {
+        backgroundColor: '#e50914',
+        padding: 2.5,
+        width: 90,
+        height: 90,
+        borderRadius: 90
+    },
+    capa: {
+        height: 85,
+        width: 85,
+        borderRadius: 85
+    },
+    logo: {
+        width: 90,
+        height: 45,
+        position: 'absolute',
+        zIndex: 10,
+        bottom: 10,
+        alignSelf: 'center'
+    },
+    gradient:{
+        width: '100%',
+        height: 80,
+        position: 'absolute',
+        zIndex: 9,
+        bottom: 0
+    },
+    previaContainer:{
+        width: '100%',
+        marginTop: 30
+    },
+    previaTitle: {
+        marginLeft: 20
     }
 })
 
