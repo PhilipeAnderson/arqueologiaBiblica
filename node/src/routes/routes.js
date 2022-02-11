372dbaeae078bb17e8876f9ddc98e7a3a4f12597
philipe_kraken <philipeacampos@hotmail.com>
2022-02-11 12:55:34 -0300

    Crud working { backend }

--- node/src/routes/routes.js
+++ node/src/routes/routes.js
@@ -2,13 +2,23 @@ const express = require('express');
 const router = express.Router();
 const Estudo = require('../models/estudo');
 
-router.get('/', (req, res) => {
-  res.json({ mensagem: 'Capturando todos os registros !' });
+router.get('/', async(req, res) => {
+  try {
+    const estudos = await Estudo.find({})
+    res.json({ error: false, estudos })
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  const id = req.params.id;
-  res.json({ mensagem: `Capturando um registro ${id}` });
+router.get('/:id', async(req, res) => {
+  try {
+    const id = req.params.id;
+    const estudo = await Estudo.findById(id)
+    res.json({ error: false, estudo })
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
 });
 
 router.post('/', async(req, res) => {
@@ -22,15 +32,25 @@ router.post('/', async(req, res) => {
   
 });
 
-router.put('/:id', (req, res) => {
-  const id = req.params.id;
-  res.json({ mensagem: `Atualizando um registro ${id}` });
+router.put('/:id', async(req, res) => {
+  try {
+    const id = req.params.id;
+    const estudo = req.body;
+    const estudoUpDate = await Estudo.findByIdAndUpdate(id, estudo);
+    res.json({ error: false, estudoUpDate });
+  }catch (err) {
+    res.json({ error: true, message: err.message});
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  res.json({ mesnagem: `Excluindo um registro ${id}` });
+router.delete('/:id', async(req, res) => {
+  try {
+    const id = req.params.id;
+    await Estudo.findByIdAndDelete(id);
+    res.json({ error: false, message: 'Deletado com Sucesso' })
+  } catch (err) {
+    res.json({ error: true, message: err.message });
+  }
 });
-
 
 module.exports = router;
