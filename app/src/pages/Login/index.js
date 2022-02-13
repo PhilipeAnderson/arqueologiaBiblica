2101500ee1f67ef03a942d1cf34f00888f8af5d8
philipe_kraken <philipeacampos@hotmail.com>
2022-02-13 20:39:47 -0300

    Changed baseURL the localhost for 10.0.2.2/3333 ! and connected frontend with backend { frontend }

--- app/src/pages/Login/index.js
+++ app/src/pages/Login/index.js
@@ -1,20 +1,61 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react';
-import {Image, View, Text} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {Image, View, Text, ActivityIndicator} from 'react-native';
 import {TextInput, Button} from 'react-native-paper';
 
+import AsyncStorage from '@react-native-community/async-storage';
+import api from '../../services/api';
 import styles from './styles';
 
 const Login = ({ navigation }) => {
+
+  // 0 Carregando - 1 Logado - 2 Deslogado
+  const [logged, setLogged] = useState(0);
   const [credenciais, setCredenciais] = useState({
     email: '',
     senha: '',
   });
 
+  const checkLogin = async () => {
+    //AsyncStorage.clear();
+    const user = await AsyncStorage.getItem('@user');
+    if(user) {
+      setLogged(1);
+      navigation.replace('Home');
+    }else{
+      setLogged(2);
+    }
+  }
+
+  const login = async () => {
+    try {
+      const response = await api.post('/usuario/login', credenciais);
+      const res = response.data;
+
+      if(res.error) {
+        alert(res.message)
+        return false;
+      } 
+
+      await AsyncStorage.setItem('@user', JSON.stringify(res.usuario));
+      navigation.replace('Home');
+
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
+  useEffect(() => {
+    checkLogin()
+  }, [])
+
   return (
     <View style={styles.bgDark}>
       <Image style={styles.logo} source={require('../../assets/logo.jpg')} />
-      <View>
+
+      {logged === 0 && <ActivityIndicator color="#fb7500" size="large" />}
+
+      {logged === 2 && <View>
         <TextInput
           mode="flat"
           label="Email ou seu número de telefone"
@@ -35,9 +76,7 @@ const Login = ({ navigation }) => {
         <Button
           mode="contained"
           style={styles.marginBottom}
-          onPress={() => {
-            navigation.navigate('Home')
-          }}>
+          onPress={() => login()}>
           Logar
         </Button>
 
@@ -52,7 +91,7 @@ const Login = ({ navigation }) => {
           O acesso está protegido pelo Google reCAPTCHA para garantir que você não
           é um robo. Saiba mais.
         </Text>
-      </View>
+      </View>}
     </View>
   );
 };
