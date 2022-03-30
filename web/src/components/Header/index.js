87493f19108218135447d0d4e07977cace15a435
philipe_kraken <philipeacampos@hotmail.com>
2022-03-29 23:56:16 -0300

    Created the component Header the application

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+export function Header() {
+  return(
+    <Container>
+      <h1>Header</h1>
+
+    </Container>
+  )
+}
---
+++ web/src/components/Header/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  width: 100%;
+  background-color: #000;
+  
+  h1 {
+    color: #f00;
+  }
+`;
