2ab9dfb1ebcb7662f5a2b3d3ae84e64f6f091986
philipe_kraken <philipeacampos@hotmail.com>
2022-05-01 22:28:57 -0300

    Screen Login and ButtonSignIn component ready styles

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,7 +1,9 @@
-import { Container, Content, InfoLogin } from './styles';
-import { Input } from '../../components/Input';
-import LogoImg from '../../assets/image/logo.jpg';
+import { Container, Content, InfoLogin, FooterLogin } from './styles';
 
+import { Input } from '../../components/Input';
+import { ButtonSignIn } from '../../components/ButtonSignIn';
+
+import LogoImg from '../../assets/image/logo.jpg';
 
 export function Login() {
   return(
@@ -11,8 +13,15 @@ export function Login() {
         <InfoLogin>
           <Input type="text" place="Entre com seu email" />
           <Input type="password" place="Digite sua senha" />
-          
+          <ButtonSignIn type="button" title="Logar"/>
         </InfoLogin>
+        <FooterLogin>
+          <p>RECUPERAR SENHA</p>
+          <small align="center">
+            O acesso está protegido pelo Google reCAPTCHA para <br />
+            garantir que você não é um robo. Saiba mais.
+          </small>
+        </FooterLogin>
       </Content>
     </Container>
   )
